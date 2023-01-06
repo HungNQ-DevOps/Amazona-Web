aa910ce9c3a4ff540ef5e96be531a4266ae8c5d8
hungnq-devObs <hp.nguyenquochung@gmail.com>
2023-01-06 15:51:51 +0700

    Add cors attribute to Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
+import cors from 'cors'
 
 dotenv.config();
 
@@ -21,6 +21,7 @@ mongoose
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
