f5053851f4d219aeeed7cff9e66eac0b3e7da429
hungnq-devObs <hp.nguyenquochung@gmail.com>
2023-01-09 15:31:08 +0700

    Update

--- app.js
+++ app.js
@@ -1,34 +1,27 @@
 import express from 'express';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import seedRouter from './routes/seedRoutes.js';
-// import productRouter from './routes/productRoutes.js';
-// import userRouter from './routes/userRoutes.js';
-// import orderRouter from './routes/orderRoutes.js';
-// import cors from 'cors'
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
+import orderRouter from './routes/orderRoutes.js';
 
 dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log('connected to db');
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//     console.log(err)
-//   });
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err)
+  });
 
 const app = express();
 
-// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use('/api/seed', seedRouter);
-// app.use('/api/products', productRouter);
-// app.use('/api/users', userRouter);
-// app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
   res.send("Home page");
 })
@@ -36,11 +29,10 @@ app.get('/api', (req, res) => {
   res.send("Hello Geek");
 })
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
